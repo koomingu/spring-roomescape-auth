d613e9aab21233c8aeb53dee4d11728ac708bc23
구민서 <minseo12379@gmail.com>
2026-05-20 20:14:39 +0900

    refactor: 웹 및 모바일 인증 통합에 따른 WebMvcConfig 의존성 수정

--- src/main/java/roomescape/auth/interceptor/WebMvcConfig.java
+++ src/main/java/roomescape/auth/interceptor/WebMvcConfig.java
@@ -4,28 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import roomescape.member.repository.MemberRepository;
 
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final MemberRepository memberRepository;
+    private final LoginCheckInterceptor loginCheckInterceptor;
 
-    public WebMvcConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public WebMvcConfig(LoginCheckInterceptor loginCheckInterceptor) {
+        this.loginCheckInterceptor = loginCheckInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
+        registry.addInterceptor(loginCheckInterceptor)
                 .addPathPatterns("/reservations/**")
-                .excludePathPatterns("/login", "/logout");
+                .excludePathPatterns("/login", "/logout", "/api/mobile/login", "/api/mobile/logout");
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver(memberRepository));
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 }
