9c6b9307b67805ce1a398f2c82e6a7ee348421a9
구민서 <minseo12379@gmail.com>
2026-05-20 20:12:17 +0900

    feat: ArgumentResolver가 세션 대신 Request Attribute를 사용하도록 통일

--- src/main/java/roomescape/auth/interceptor/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/auth/interceptor/LoginMemberArgumentResolver.java
@@ -1,24 +1,15 @@
 package roomescape.auth.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import roomescape.auth.controller.AuthController;
 import roomescape.exception.UnauthorizedException;
 import roomescape.member.domain.Member;
-import roomescape.member.repository.MemberRepository;
 
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
-
-    private final MemberRepository memberRepository;
-
-    public LoginMemberArgumentResolver(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -32,15 +23,13 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        HttpSession session = request.getSession(false);
 
-        if (session == null) {
-            throw new UnauthorizedException("로그인 세션이 만료되었습니다.");
+        Member member = (Member) request.getAttribute(LoginCheckInterceptor.LOGIN_MEMBER_REQUEST_KEY);
+
+        if (member == null) {
+            throw new UnauthorizedException("인증 정보가 존재하지 않습니다.");
         }
 
-        Long memberId = (Long) session.getAttribute(AuthController.SESSION_KEY);
-
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new UnauthorizedException("존재하지 않는 회원입니다."));
+        return member;
     }
 }
